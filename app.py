8fea1ae2fe830aef4f35908314577b71708f109b
synapsephotos <135157942+synapsephotos@users.noreply.github.com>
2026-01-13 11:57:22 +0100

    Update app.py

--- app.py
+++ app.py
@@ -34,7 +34,7 @@ def run_flask():
 
 def prepare_for_reverse(text: str) -> str:
     if not text: return ""
-    text = text.replace(", ", " ,")
+    text = text.replace(", ", " ,").replace("? ", " ?")
     chars_to_move = ".?!\""
     stripped_text = text.rstrip(chars_to_move)
     punctuation_tail = text[len(stripped_text):]
